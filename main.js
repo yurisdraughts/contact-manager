68eec42bba36294743e759aca9b53a2f4838f345
Yuri M <yurisdraughts@gmail.com>
2023-01-31 09:53:45 +0300

    Added background

--- main.js
+++ main.js
@@ -1,6 +1,21 @@
 import { createApp } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js';
 import { ContactManager } from './modules/ContactManager.js';
 
+document.querySelector('.background').addEventListener('mousemove', function(event) {
+    const mousePosition = Math.hypot(
+        event.clientX / this.offsetWidth,
+        event.clientY / this.offsetHeight
+    );
+
+    const basePercent = 25,
+        percentRange = 50,
+        adjustablePercent = percentRange * mousePosition;
+
+    const gradientPercent = basePercent + adjustablePercent;
+
+    this.style.setProperty('--middle-point', `${gradientPercent}%`);
+});
+
 const contactManager = new ContactManager();
 
 createApp({
